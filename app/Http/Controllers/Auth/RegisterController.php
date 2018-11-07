19f23329112c006d2dd0468f686558bb1a8d23ff
OrelSBinoklem <robingoode2@mail.ru>
2018-11-07 19:59:28 +0200

    restore login with github, create user add request for return email_verified_at

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -79,10 +79,11 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        return User::find($user->id);
     }
 }
